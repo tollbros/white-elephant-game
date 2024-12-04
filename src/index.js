b2fe14b73ccb7c4d6a265deb5a010a00085e308e
Sara Meixner <smeixner@tollbrothers.com>
2024-12-04 11:28:10 -0500

    fix: update package-lock and file references so github can deploy to pages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App'; // Ensure the path is correct
+import App from './App.js';
 import './index.css'; // Ensure Tailwind styles are included
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
